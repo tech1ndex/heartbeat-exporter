cd4de6e2977ac00f006bd45e4e3338a27af8573d
Victor Bajada <bajada.victor@gmail.com>
2024-04-15 13:38:27 -0400

    Add Initial Working Version

--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
 from prometheus_client import start_http_server, Gauge
 import requests
 import time
+import argparse
+
+# Define Arguments
+parser = argparse.ArgumentParser(description='A simple HTTP connection tester written in Python.')
+parser.add_argument("-u", "--url", help="A URL to test against", required=True)
+args = parser.parse_args()
 
 # Define Prometheus metrics
 URL_STATUS = Gauge('url_status', 'Status of the URL (1=reachable, 0=unreachable)')
-
+# Define parameters of URL Check
 def check_url(url):
     try:
         response = requests.get(url)
         return 1 if response.status_code == 200 else 0
     except:
         return 0
-
+# Define Updating Metrics
 def update_metrics(url):
     status = check_url(url)
     URL_STATUS.set(status)
 
+# Main function to update metrics
 if __name__ == '__main__':
-    # URL to check
-    url_to_check = 'http://example.com'
-
-    # Start the Prometheus metrics server
+    url_to_check = str(args.url)
     start_http_server(8000)
-
-    # Main loop to periodically update metrics
     while True:
         update_metrics(url_to_check)
         time.sleep(60)
